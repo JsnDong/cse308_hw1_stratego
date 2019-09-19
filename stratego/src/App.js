66ef38f1aae1349a5bf1fee90b16a34db4a57b98
Jason Dong <jasondong1294@gmail.com>
2019-09-19 19:22:09 -0400

    removed style for app.js

--- stratego/src/App.js
+++ stratego/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
 
 import Login from './components/login.jsx';
 import SignUp from './components/signup.jsx';
