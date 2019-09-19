f8267e954b5f97a1682e7fcabfc9234f99e6fbae
Jason Dong <jasondong1294@gmail.com>
2019-09-19 15:49:10 -0400

    refactored App so that components, objects, and functions have their own files

--- stratego/src/App.js
+++ stratego/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Tile from './Tile.jsx';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DIMENSION = 10;
+const IMPASSABLES = ["(2,4)","(2,5)", "(3,4)", "(3,5)",
+                     "(6,4)","(6,5)", "(7,4)", "(7,5)"];
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: null
+    }
+  }
+
+  render() {
+    const board = [];
+
+    for (let i = 0; i < DIMENSION; i++) {
+      let board_row = [];
+      for (let j = 0; j < DIMENSION; j++) {
+        let impassable = IMPASSABLES.includes("(" + i + "," + j + ")");
+        board_row.push(<Tile x={i} y={j} impassable={impassable}/>);
+      }
+      board.push(<div className="board_row">{board_row}</div>);
+    }
+
+    return board;
+  }
 }
 
-export default App;
+export default Board
