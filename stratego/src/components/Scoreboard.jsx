3cc653b2404cdf1a5d0a7b1018dc45b1203c3706
Jason Dong <jasondong1294@gmail.com>
2019-09-27 19:32:25 -0400

    Scoreboard implemented, live tracking of how many pieces each player has

--- stratego/src/components/Scoreboard.jsx
+++ stratego/src/components/Scoreboard.jsx
@@ -3,21 +3,25 @@ import '../stylesheets/App.css';
 
 class Scoreboard extends React.Component {
     render() {
-        let scoreboard = [];
-        let ranks = Object.keys(this.state.pieces);
-        for (let i = 0; i < ranks.length; i++) {
-            scoreboard.push(<div className="scoreboard_row">
-                                {ranks[i] + "\t" + this.state.pieces[ranks[i]].BLUE + "\t" + this.state.pieces[ranks[i]].RED}
-                             </div>);
+        const {scoreboard} = this.props
+        let table = []
+        let table_row;
+        for (let row = 0; row < scoreboard.length; row++) {
+            table_row = []
+            for (let col = 0; col < scoreboard[0].length; col++) {
+                if (!row) {
+                    table_row.push(<th>{scoreboard[row][col]}</th>)
+                } else {
+                    table_row.push(<td>{scoreboard[row][col]}</td>)
+                }
+            }
+            table.push(<tr>{table_row}</tr>)
         }
 
         return (
-            <div className="scoreboard">
-                <div className="scoreboard_header">
-                        {"BLUE \t RED"}
-                </div>
-                {scoreboard}
-            </div>
+            <table className="scoreboard">
+                {table}
+            </table>
         );
     }
 }
