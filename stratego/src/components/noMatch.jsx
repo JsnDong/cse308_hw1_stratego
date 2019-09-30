ad0329ec02b43f2c33c851aaa7ac33c783ed1411
Agnieszka Gawrys <agnieszka.gawrys@stonybrook.edu>
2019-09-29 22:15:48 -0400

    styled error page

--- stratego/src/components/noMatch.jsx
+++ stratego/src/components/noMatch.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import '../stylesheets/errorPage.css';
+import titleTxt from '../stylesheets/404.svg';
+import { Button } from 'react-bootstrap';
 
 export class noMatch extends Component {
 	render() {
 		return (
-			<div>
-				<h1>404</h1>
-				<Link to="/login" style={{ textDecoration: 'none' }}>
-					<button>Log in to play!</button>
+			<div className="stratego">
+				<img style={{height: 300, width: '100%', backgroundColor: 'transparent' }} src={titleTxt} />
+				<Link to="/login" className="links" style={{ textDecoration: 'none' }}>
+					<Button style={{ backgroundColor: '#7566BD' }} class="btn btn-primary btn-lg">
+						Login to Play!
+					</Button>
 				</Link>
+				
 			</div>
 		);
 	}
